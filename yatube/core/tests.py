d80bd393594168053b57550671f4cf89f642d7fc
Àëêåêñàíäð Âàñèëü÷óê <alexandrvsko@yandex.ru>
2023-02-13 00:57:06 +0300

    core tests change

--- yatube/core/tests.py
+++ yatube/core/tests.py
@@ -4,17 +4,12 @@ from django.test import TestCase, Client
 
 
 class ViewTestClass(TestCase):
-
-    def setUp(self) -> None:
-        self.csrf_user = Client(enforce_csrf_checks=True)
-
     def test_error_page(self):
         """Проверка недоступности страницы с кодом 404"""
         self.assertEqual(
             self.client.get('/nonexist-page/').status_code,
             HTTPStatus.NOT_FOUND,
         )
-        self.assertEqual(self.csrf_user.get(reverse('posts:index')).status_code, HTTPStatus.FORBIDDEN)
 
     def test_error_template(self):
         """Проверка использования верного шаблона"""
